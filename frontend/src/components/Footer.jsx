1f7e68983b37d2fb905ac122368f297043a4898d
Miguel Buitrago <miguelcatholic@gmail.com>
2024-09-04 14:59:57 -0500

    subiendo cambios

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import {FaFacebook, FaInstagram,FaTiktok} from 'react-icons/fa'
+import {FaFacebook, FaInstagram,FaTiktok, FaGithub} from 'react-icons/fa'
 
 
 const Footer = () => {
   return (
-    <div>
+    <div id='contenedor'>
+      <a href='https://www.facebook.com/profile.php?id=100089510108006&locale=es_LA'>
+      <div id="caja"><FaFacebook/></div>
+      </a>
+      <a href='https://www.instagram.com/miguelangelbuitragorico/reels/?next=%2F'>
+      <div id="caja"><FaInstagram/></div>
+      </a>
+      <a href='https://www.tiktok.com/tag/toktok?lang=es'>
+      <div id="caja"><FaTiktok/></div>
+      </a>
+      <a href='https://github.com/MiguelitoB205'>
+      <div id="caja"><FaGithub/></div>
+      </a>
       
     </div>
   )
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import styles from './Form.module.css'
+
+const Form = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [isError, setIsError] = useState(false);
+    const [showCard, setShowCard] = useState(false);
+
+    const isValidName = name.length > 5;
+    const isValidEmail = email.includes("@") && email.includes(".com");
+
+    function signUpUpser(e) {
+        e.preventDefault();
+
+        if(isValidName && isValidEmail) {
+            setIsError(false);
+            setShowCard(true);
+        } else{ 
+            setIsError(true)
+        }
+    }
+
+    const onNameChange = (e) => {
+        resetValidationStates();
+        setName(e.target.value);
+    }
+
+    const onChangeEmail = (e) => {
+        resetValidationStates();
+        setEmail(e.target.value);
+    }
+
+    const resetValidationStates = () => {
+        setShowCard(false);
+        setIsError(false);
+    }
+  return (
+    <>
+      <form onSubmit={signUpUpser}>
+        <div>
+            <input className={styles.input}
+             type="text"
+             placeholder='Name'
+             value={name}
+             onChange={onNameChange}
+             onFocus={resetValidationStates} 
+             />
+        </div>
+        <div>
+            <input className={styles.input}
+            type="text"
+            placeholder='Email'
+            value={email}
+            onChange={onChangeEmail}
+            onFocus={resetValidationStates} />
+        </div>
+        <div>
+            <button type='submit'>Submit</button>
+        </div>
+        {isError && <h1>Error, please, try again!</h1>}
+      </form>
+      {showCard && (
+        <div className={styles.card}>
+            <div>Thank you {name}!!, we'll contact you by email!!! </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Form
